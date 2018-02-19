7726d04d20291674f962bfcb5cc494934a762ff2
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-02-19 16:53:10 +0000

    [tests/ar] Use normal here-documents in chktest.sh

--- tests/ar/execute/chktest.sh
+++ tests/ar/execute/chktest.sh
@@ -11,19 +11,19 @@ rm -rf file* master.a
 uid=`id -u`
 gid=`id -g`
 
-cat <<"EOF" >master.a
+cat <<EOF >master.a
 !<arch>
-file1           315532800   $uid  $gid  100644  79        `
+file1           315532800   $uid  $gid  100644  79        \`
 This is the first file,
 and it should go in the
 first position in the archive.
 
-file2           315532800   $uid  $gid  100644  125       `
+file2           315532800   $uid  $gid  100644  125       \`
 But this other one is the second one,
 and it shouldn't go in the first position
 because it should go in the second position.
 
-file3           315532800   $uid  $gid  100644  118       `
+file3           315532800   $uid  $gid  100644  118       \`
 and at the end, this is the last file
 that should go at the end of the file,
 thus it should go in the third position.
