4b15d284ea6576d5229c43162e4c6dc38a386266
Quentin Rameau <quinq@fifth.space>
2018-02-21 17:24:54 +0100

    fix sysincludes internal path
    We don't use TARGET anymore, which can be assembled with ARCH and SYS

--- inc/sysincludes.def.h
+++ inc/sysincludes.def.h
@@ -1,6 +1,6 @@
 char *sysincludes[] = {
 	PREFIX "/include/scc/" ,
-	PREFIX "/include/scc/bits/" TARGET "/",
+	PREFIX "/include/scc/bits/" ARCH "-" ABI "/",
 	/* configure below your standard sys include paths */
 	PREFIX "/include/",
 	PREFIX "/local/include/",
