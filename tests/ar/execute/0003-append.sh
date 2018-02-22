0f5c99c02edde1295a1be0592aaf6844f7c9cacc
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-02-22 15:14:33 +0000

    [tests/ar] Fix 0003-append.sh
    master.a already has files with file? names.

--- tests/ar/execute/0003-append.sh
+++ tests/ar/execute/0003-append.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-trap "rm -f file.a f1 f2 f3 $tmp1 $tmp2" 0 2 3
+trap "rm -f file* f1 f2 f3 $tmp1 $tmp2" 0 2 3
 
 tmp1=`mktemp`
 tmp2=`mktemp`
@@ -10,24 +10,24 @@ tmp2=`mktemp`
 ###########################################################################
 #Append generated files at once to an existing archive
 
-echo First > file1
-echo Second > file2
-echo Third > file3
+echo First > file-1
+echo Second > file-2
+echo Third > file-3
 
 cp master.a file.a
-ar -qv file.a file1 file2 file3
+ar -qv file.a file-1 file-2 file-3
 
-ar -t file.a file1 file2 file3 > $tmp1
+ar -t file.a file-1 file-2 file-3 > $tmp1
 
 cat <<EOF > $tmp2
-file1
-file2
-file3
+file-1
+file-2
+file-3
 EOF
 
 cmp $tmp1 $tmp2
 
-ar -p file.a file1 file2 file3 > $tmp1
+ar -p file.a file-1 file-2 file-3 > $tmp1
 
 cat <<EOF > $tmp2
 First
