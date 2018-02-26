e1f8c5e769f2ced177ce201bd11b122ec2f4876b
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-02-26 07:42:20 +0000

    [tests/ar] Remove generation of master.a from chktest.sh
    This work is done in mkmaster.sh

--- tests/ar/execute/chktest.sh
+++ tests/ar/execute/chktest.sh
@@ -6,28 +6,7 @@ stty tabs
 tabs 40
 ulimit -c 40
 rm -f test.log
-rm -rf file* master.a
-
-uid=`id -u`
-gid=`id -g`
-
-cat <<EOF >master.a
-!<arch>
-file1           315532800   `printf "%-6d" $uid``printf "%-6d" $gid`100644  79        \`
-This is the first file,
-and it should go in the
-first position in the archive.
-
-file2           315532800   `printf "%-6d" $uid``printf "%-6d" $gid`100644  125       \`
-But this other one is the second one,
-and it shouldn't go in the first position
-because it should go in the second position.
-
-file3           315532800   `printf "%-6d" $uid``printf "%-6d" $gid`100644  118       \`
-and at the end, this is the last file
-that should go at the end of the file,
-thus it should go in the third position.
-EOF
+rm -rf file*
 
 for i in *-*.sh
 do
