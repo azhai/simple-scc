b5356e984cde1e77d9cc9aa92e0b417a4f217908
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-03-14 21:03:03 +0100

    [ld/coff32] Add basic support for files

--- ld/coff32.c
+++ ld/coff32.c
@@ -1,6 +1,8 @@
 static char sccsid[] = "@(#) ./ld/coff32.c";
 
+#include <assert.h>
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,12 +13,75 @@ static char sccsid[] = "@(#) ./ld/coff32.c";
 #include "../inc/scc.h"
 #include "ld.h"
 
-static int
-pass1(char *fname, char *member, FILE *fp)
+static int (*unpack)(unsigned char *, char *, ...);
+static long strtbl, symtbl, sectbl;
+
+static FILHDR *
+getfhdr(unsigned char *buff, FILHDR *hdr)
 {
+	int n;
+
+	n = (*unpack)(buff,
+	          "sslllss",
+	          &hdr->f_magic,
+	          &hdr->f_nscns,
+	          &hdr->f_timdat,
+	          &hdr->f_symptr,
+	          &hdr->f_nsyms,
+	          &hdr->f_opthdr,
+	          &hdr->f_flags);
+	assert(n == FILHSZ);
+	return hdr;
 }
 
-static int
+static void
+pass1(char *fname, char *member, FILE *fp)
+{
+	unsigned char buff[FILHSZ];
+	FILHDR *hdr;
+	Obj *obj;
+	long siz, pos = ftell(fp);
+	char *str;
+
+	if (fread(buff, FILHSZ, 1, fp) != 1)
+		goto bad_file;
+
+	if ((hdr = malloc(sizeof(*hdr))) == NULL)
+		goto out_of_memory;
+
+	obj = newobj(fname);
+	obj->hdr = getfhdr(buff, hdr);
+
+	/* TODO: Check overflow */
+	strtbl = pos + hdr->f_symptr + hdr->f_nsyms* SYMESZ;
+	symtbl = pos + hdr->f_symptr;
+	sectbl = pos + FILHSZ + hdr->f_opthdr;
+
+	if (fseek(fp, strtbl, SEEK_SET) == EOF)
+		goto bad_file;
+
+	if (fread(buff, 4, 1, fp) != 1)
+		goto bad_file;
+
+	(*unpack)(buff, "l", &siz);
+
+	if (siz > SIZE_MAX || (str = malloc(siz)) == NULL)
+		goto out_of_memory;
+
+	if (fread(str, siz, 1, fp) != 1)
+		goto bad_file;
+
+	obj->strtbl = str;
+
+out_of_memory:
+	die("ld: out of memory");
+bad_file:
+	if (ferror(fp))
+		die("ld: %s: %s", fname, strerror(errno));
+	die("ld: %s: corrupted file", fname);
+}
+
+static void
 pass2(char *fname, char *member, FILE *fp)
 {
 }
---
+++ ld/obj.c
@@ -0,0 +1,33 @@
+static char sccsid[] = "@(#) ./ld/obj.c";
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "../inc/scc.h"
+#include "ld.h"
+
+Obj *objlst;
+static Obj *tail;
+
+Obj *
+newobj(char *fname)
+{
+	Obj *obj;
+	char *s;
+	size_t len = strlen(fname);
+
+	obj = malloc(sizeof(*obj));
+	s = malloc(len+1);
+	if (!obj || !s)
+		die("ld: out of memory");
+
+	obj->fname = memcpy(s, fname, len);
+	obj->next = NULL;
+
+	if (!objlst)
+		tail = objlst = obj;
+	else
+		tail->next = obj;
+
+	return obj;
+}
