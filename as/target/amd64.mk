b79ca20491ce1570c50880a476106d8bf5c46acc
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-05-16 20:01:22 +0100

    [as] Simplify build system
    Get rid of gendep.sh. This was very stupid since all the rules can be
    encoded directly in the Makefile. If you don't want to build everything
    then you only have to run make with a different target than all.

--- as/target/amd64.mk
+++ as/target/amd64.mk
@@ -1,9 +1,10 @@
-
-amd64_LST = target/x86/amd64tbl.o target/x86/amd64.o target/x86/ins.o
-
-$(amd64_LST): target/x86/proc.h
+AMD64_OBJ = $(OBJ) target/x86/amd64tbl.o target/x86/amd64.o target/x86/ins.o
 
 target/x86/amd64tbl.c: gentbl.awk target/x86/x86.dat target/x86/rules.dat
 	./gentbl.sh -f x86 -c amd64
 
-OBJ-amd64 = $(OBJ) $(amd64_LST)
+as-amd64: $(AMD64_OBJ) $(LIBDIR)/libscc.a
+	$(CC) $(SCC_LDFLAGS) $(AMD64_OBJ) -lscc -o $@
+
+debug:
+	echo $(AMD64_OBJ)
--- as/target/z80.mk
+++ as/target/z80.mk
@@ -1,9 +1,7 @@
-
-Z80_LST = target/x80/z80tbl.o target/x80/z80.o target/x80/ins.o
-
-$(Z80_LST): target/x80/proc.h
+Z80_OBJ = $(OBJ) target/x80/z80tbl.o target/x80/z80.o target/x80/ins.o
 
 target/x80/z80tbl.c: gentbl.awk target/x80/x80.dat target/x80/rules.dat
 	LC_ALL=C ./gentbl.sh -f x80 -c z80
 
-OBJ-z80 = $(OBJ) $(Z80_LST)
+as-z80: $(OBJ) $(Z80_OBJ) $(LIBDIR)/libscc.a
+	$(CC) $(SCC_LDFLAGS) $(Z80_OBJ) -lscc -o $@
