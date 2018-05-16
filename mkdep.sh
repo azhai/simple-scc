874cce9c8746da398c57e021ea6ee740ca71e1cb
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-05-16 20:45:36 +0100

    Fix inclusion dependencies generation
    .c dependencies do nothing.

--- mkdep.sh
+++ mkdep.sh
@@ -6,8 +6,8 @@ trap "rm -f $$.tmp" 0 2 3
 (sed '/^#deps/q' Makefile
 for i in `find . -name '*.c'`
 do
-	file=${i#./}
-	dir=`dirname $i | sed -e 's,/*$,,' -e 's,\./,,'`
+	file=`echo $i | sed -e 's,^./,,' -e 's/\.c$/.o/'`
+	dir=`dirname $i | sed 's,/*$,,'`
 	sed -n '/#include "/ s,#include "\(.*\)",'"$file: $dir"'/\1,p' $i
 done |
 sort) > $$.tmp && mv $$.tmp Makefile
