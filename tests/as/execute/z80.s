2b1d0de25b9fa7aea6d32bf7d423d24e08becbe7
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-05-26 01:01:59 +0100

    Fix syntax of jumps
    'JP [HL]' and similar imply that the contents pointed by [HL] are read,
    and that is the address where the CPU jumps to. This is not the case.
    That syntax would mean that a byte is loaded from the address pointed by
    HL, and a byte is not a valid address.
    
    The actual behaviour is to move the value of HL to the PC, which means
    that there is no indirection, and that the syntax should be 'JP HL'.
    
    The same applies to 'JP [IX]' and 'JP [IY]'.

--- tests/as/execute/z80.s
+++ tests/as/execute/z80.s
@@ -757,7 +757,7 @@ L6:	JR	C,$L6	/ 38 02
 	POP	IX	/ DD E1
 	EX	(SP),IX	/ DD E3
 	PUSH	IX	/ DD E5
-	JP	(IX)	/ DD E9
+	JP	IX	/ DD E9
 	LD	SP,IX	/ DD F9
 	SBC	A,$64	/ DE 40
 	RST	$24	/ DF
@@ -770,7 +770,7 @@ L6:	JR	C,$L6	/ 38 02
 	AND	A,$16	/ E6 10
 	RST	$32	/ E7
 	RET	PE	/ E8
-	JP	(HL)	/ E9
+	JP	HL	/ E9
 	JP	PE,$16384	/ EA 00 40
 	EX	DE,HL	/ EB
 	CALL	PE,$16384	/ EC 00 40
@@ -1130,7 +1130,7 @@ L6:	JR	C,$L6	/ 38 02
 	POP	IY	/ FD E1
 	EX	(SP),IY	/ FD E3
 	PUSH	IY	/ FD E5
-	JP	(IY)	/ FD E9
+	JP	IY	/ FD E9
 	LD	SP,IY	/ FD F9
 	CP	A,$32	/ FE 20
 	RST	$56	/ FF
