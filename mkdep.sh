446dc0208959152bad0bb3ed1cb5f037d247d805
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-08-15 18:34:01 +0100

    Move generated dependencias to deps.mk
    It makes easier to read the base Makefiles.

--- mkdep.sh
+++ mkdep.sh
@@ -3,11 +3,11 @@
 set -e
 trap "rm -f $$.tmp" 0 2 3
 
-(sed '/^#deps/q' Makefile
+(sed '/^#deps/q' deps.mk
 for i in `find . -name '*.c'`
 do
 	file=`echo $i | sed -e 's,^./,,' -e 's/\.c$/.o/'`
 	dir=`dirname $i | sed 's,/*$,,'`
 	sed -n '/#include "/ s,#include "\(.*\)",'"$file: $dir"'/\1,p' $i
 done |
-LC_ALL=C sort -s) > $$.tmp && mv $$.tmp Makefile
+LC_ALL=C sort -s) > $$.tmp && mv $$.tmp deps.mk
