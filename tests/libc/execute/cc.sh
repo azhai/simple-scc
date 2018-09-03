aec67e43c52fcc1857d00d498d82189bbf0f680a
Roberto E. Vargas Caballero <k0ga@shike2.com>
2018-09-03 15:07:10 +0100

    [tests/libc] Add 0007-signal test

--- tests/libc/execute/cc.sh
+++ tests/libc/execute/cc.sh
@@ -34,13 +34,17 @@ out=${out:-a.out}
 root=${root:-$SCCPREFIX}
 inc=$root/include/scc
 arch_inc=$root/include/scc/bits/$abi
+sys_inc=$root/include/scc/bits/$sys
 lib=$root/lib/scc/${abi}-${sys}
 obj=${1%.c}.o
+
+includes="-nostdinc -I$inc -I$arch_inc -I$sys_inc"
+flags="-std=c99 -g -w -fno-stack-protector --freestanding -static"
 
 if ! gcc -nopie 2>&1 | grep unrecogn >/dev/null
 then
 	pie=-nopie
 fi
 
-gcc -std=c99 -g -w $pie -fno-stack-protector --freestanding -std=c99 -static -nostdinc -I$inc -I$arch_inc -c $1
+gcc $flags $pie $includes -c $1
 ld -g $pie -z nodefaultlib -static -L$lib $lib/crt.o $obj -lc -o $out
