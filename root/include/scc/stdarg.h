f01477198b79f9944f8c6e18f90a18c7f70d1365
sin <sin@2f30.org>
2018-10-08 13:38:45 +0100

    Use typedef instead of #define for va_list

--- root/include/scc/stdarg.h
+++ root/include/scc/stdarg.h
@@ -1,7 +1,7 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-#define va_list __builtin_va_list
+typedef __builtin_va_list va_list;
 #define va_start(ap, last) __builtin_va_start(ap, last)
 #define va_end(ap) __builtin_va_end(ap)
 #define va_copy(to, from) __builtin_va_copy(to, from)
