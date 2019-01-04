a4f55bc2a984f51aa2afef82f7f6257dece7a09d
Roberto E. Vargas Caballero <k0ga@shike2.com>
2019-01-04 15:33:43 +0000

    Remove libcoff32
    The work of this library is going to be done by libmach.

--- src/ld/deps.mk
+++ src/ld/deps.mk
@@ -1,12 +1,5 @@
 
 #deps
-coff32.o: $(INCDIR)/scc/scc/coff32/aouthdr.h
-coff32.o: $(INCDIR)/scc/scc/coff32/coff32.h
-coff32.o: $(INCDIR)/scc/scc/coff32/filehdr.h
-coff32.o: $(INCDIR)/scc/scc/coff32/scnhdr.h
-coff32.o: $(INCDIR)/scc/scc/coff32/syms.h
-coff32.o: $(INCDIR)/scc/scc/scc.h
-coff32.o: ld.h
 main.o: $(INCDIR)/scc/scc/ar.h
 main.o: $(INCDIR)/scc/scc/scc.h
 main.o: $(INCDIR)/scc/scc/syslibs.h
--- src/libcoff32/coff32_unpack_aout.c
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-
-#include <scc/coff32/aouthdr.h>
-#include <scc/coff32/coff32.h>
-
-void
-coff32_unpack_aout(packfun fun, unsigned char *buff, AOUTHDR *ent)
-{
-}
