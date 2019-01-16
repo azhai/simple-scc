27e63021261aee56e2195e328c9e4ab61bce8870
Roberto E. Vargas Caballero <k0ga@shike2.com>
2019-01-16 22:34:33 +0000

    [libc] Include libc.h in files using _flsbuf
    This definitions was lost.

--- src/libc/stdio/fclose.c
+++ src/libc/stdio/fclose.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "../syscall.h"
+#include "../libc.h"
 #undef fclose
 
 int
--- src/libc/stdio/setvbuf.c
+++ src/libc/stdio/setvbuf.c
@@ -1,6 +1,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "../libc.h"
 #undef setvbuf
 
 int
