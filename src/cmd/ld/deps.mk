6c1eac285301e17024452e18e95e882ed758f3b0
Roberto E. Vargas Caballero <k0ga@shike2.com>
2019-02-10 12:20:44 +0000

    Fix dep generation

--- src/cmd/ld/deps.mk
+++ src/cmd/ld/deps.mk
@@ -1,8 +1,5 @@
-
 #deps
-main.o: $(INCDIR)/scc/scc/ar.h
-main.o: $(INCDIR)/scc/scc/scc.h
-main.o: $(INCDIR)/scc/scc/syslibs.h
-main.o: ld.h
-obj.o: $(INCDIR)/scc/scc/scc.h
-obj.o: ld.h
+./main.o: $(INCDIR)/scc/scc/ar.h
+./main.o: $(INCDIR)/scc/scc/mach.h
+./main.o: $(INCDIR)/scc/scc/scc.h
+./main.o: $(INCDIR)/scc/scc/syslibs.h
--- src/libscc/deps.mk
+++ src/libscc/deps.mk
@@ -1,12 +1,11 @@
 #deps
-alloc.o: $(INCDIR)/scc/scc/scc.h
-bpack.o: $(INCDIR)/scc/scc/scc.h
-casecmp.o: $(INCDIR)/scc/scc/scc.h
-debug.o: $(INCDIR)/scc/scc/scc.h
-die.o: $(INCDIR)/scc/scc/scc.h
-lpack.o: $(INCDIR)/scc/scc/scc.h
-newitem.o: $(INCDIR)/scc/scc/scc.h
-xcalloc.o: $(INCDIR)/scc/scc/scc.h
-xmalloc.o: $(INCDIR)/scc/scc/scc.h
-xrealloc.o: $(INCDIR)/scc/scc/scc.h
-xstrdup.o: $(INCDIR)/scc/scc/scc.h
+./alloc.o: $(INCDIR)/scc/scc/scc.h
+./bpack.o: $(INCDIR)/scc/scc/scc.h
+./casecmp.o: $(INCDIR)/scc/scc/scc.h
+./debug.o: $(INCDIR)/scc/scc/scc.h
+./die.o: $(INCDIR)/scc/scc/scc.h
+./newitem.o: $(INCDIR)/scc/scc/scc.h
+./xcalloc.o: $(INCDIR)/scc/scc/scc.h
+./xmalloc.o: $(INCDIR)/scc/scc/scc.h
+./xrealloc.o: $(INCDIR)/scc/scc/scc.h
+./xstrdup.o: $(INCDIR)/scc/scc/scc.h
