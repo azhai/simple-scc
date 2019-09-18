576703987fa1dbc4d63daa490145b78b249669d3
Roberto E. Vargas Caballero <k0ga@shike2.com>
2019-09-18 21:21:50 +0200

    [as-powerpc] Add skeleton for different instruction formats

--- src/cmd/as/target/powerpc/ins.c
+++ src/cmd/as/target/powerpc/ins.c
@@ -1,9 +1,11 @@
+#include <stdlib.h>
+
 #include <scc/scc.h>
 
 #include "../../as.h"
 
 int
-match(Op *op, Node **args)
+match(Op *Op, Node **args)
 {
 	return 0;
 }
@@ -11,4 +13,161 @@ match(Op *op, Node **args)
 Node *
 moperand(void)
 {
+	abort();
+}
+
+void
+i_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+b_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+sc_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+d_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+ds_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+dq_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+x_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xl_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xfx_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xlfdorm_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xx1_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xx2_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xx3_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xx4_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xs_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+xo_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+a_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+m_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+md_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+mds_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+va_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+vc_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+vx_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+evs_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+z22_form(Op *op, Node **args)
+{
+	abort();
+}
+
+void
+z23_form(Op *op, Node **args)
+{
+	abort();
 }
