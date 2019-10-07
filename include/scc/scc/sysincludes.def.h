2974c0654f4634a58fb6d6d588dd4eebd143a383
Quentin Rameau <quinq@fifth.space>
2019-10-07 22:52:32 +0200

    [cc] Fix default include paths

--- include/scc/scc/sysincludes.def.h
+++ include/scc/scc/sysincludes.def.h
@@ -1,6 +1,7 @@
 /* configure below your standard sys include paths */
 char *sysincludes[] = {
-	"%p/include/scc/",
-	"%p/include/scc/bits/%a-%s",
+	"%p/include/scc/bits/%a",
+	"%p/include/scc/bits/%s",
+	"%p/include/",
 	NULL
 };
