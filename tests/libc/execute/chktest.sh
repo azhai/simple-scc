589eb3a61a8ee46e857aa2e20d9f7014923dc00e
zerous Naveen Narayanan <zerous@nocebo.space>
2019-11-16 13:43:52 +0100

    Remove generated binaries
    Update Makefile
    Update gensys.sh

--- tests/libc/execute/chktest.sh
+++ tests/libc/execute/chktest.sh
@@ -3,14 +3,15 @@
 file=${1?' empty input file'}
 tmp1=`mktemp`
 tmp2=`mktemp`
-trap "rm -f a.out *.o $tmp1 $tmp2; exit" 0 1 2 3 15
+bins=`awk '{print $1}' libc-tests.lst`
+trap "rm -f *.o $bins $tmp1 $tmp2; exit" 0 1 2 3 15
 ulimit -c 0
 rm -f test.log
 
 while read i state
 do
 	state=${state:-"\t"}
-	rm -f a.out *.o $tmp1 $tmp2
+	rm -f *.o $tmp1 $tmp2
 
 	(echo $i
 	 ./cc.sh $CFLAGS -o $i $i.c
