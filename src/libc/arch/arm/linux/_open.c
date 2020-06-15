ad78b7323c3d0bbd562730c6e17814cec9c2d68a
Naveen Narayanan <zerous@simple-cc.org>
2020-06-15 14:51:26 +0200

    libc: Fix function prototype (arm32) - open, openat
    The change was introduced in commit 687ff1f6 but went uncaught since
    we didn't build scc for arm until now.
    
    Signed-off-by: Naveen Narayanan <zerous@simple-cc.org>

--- src/libc/arch/arm/linux/_open.c
+++ src/libc/arch/arm/linux/_open.c
@@ -7,7 +7,7 @@
 extern int _openat(int fd, const char *fname, int flags, int mode);
 
 int
-_open(const char *fname, int flags)
+_open(const char *fname, int flags, int mode)
 {
 	return _openat(AT_FDCWD, fname, flags, mode);
 }
