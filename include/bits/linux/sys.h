7b2947de49547c2585eea8378d07b015465d0d6b
Roberto E. Vargas Caballero <k0ga@shike2.com>
2020-06-19 17:45:14 +0200

    libc: Fix linux sys.h
    These values were wrong and they affected to fopen.

--- include/bits/linux/sys.h
+++ include/bits/linux/sys.h
@@ -2,9 +2,9 @@
 #define O_WRONLY  0x00000001
 #define O_RDWR    0x00000002
 
-#define O_TRUNC   0x00000400
-#define O_APPEND  0x00000008
-#define O_CREAT   0x00000200
+#define O_TRUNC   0x00000200
+#define O_APPEND  0x00000400
+#define O_CREAT   0x00000040
 
 typedef int pid_t;
 
