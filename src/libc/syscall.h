ae0d87a19f53e5e8690e4a41536b5a825a3d9787
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-08-28 11:52:03 +0200

    libc: Add _access() and _lseek() syscalls to syscall.h
    These syscalls are used in several functions without a
    declaration.

--- src/libc/syscall.h
+++ src/libc/syscall.h
@@ -7,3 +7,5 @@ extern int _open(const char *, int, int);
 extern int _read(int, void *, size_t);
 extern int _unlink(const char *);
 extern int _write(int, void *, size_t);
+extern int _access(const char *, int);
+extern long _lseek(int, long, int);
