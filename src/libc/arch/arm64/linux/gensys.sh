b1a932d399555b5980423a59c3f74e9f42872934
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-09-14 14:47:48 +0200

    libc: Update arch/arm/linux
    This code is being updated out of the tree of scc and
    it is time to synchroniza both copies now.

--- src/libc/arch/arm64/linux/gensys.sh
+++ src/libc/arch/arm64/linux/gensys.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#
 # This job is very easy because app and kernel ABI are identical
 # until the 4th parameter, so we only have to set the syscall
 # number in rax
 
-sed 's/[ 	]*#.*//
-     /^$/d' syscall.lst |
+sed -n "
+     s/[ 	]*#.*//
+     /$1/p" syscall.lst |
 while read num name
 do
 cat <<EOF > $name.s
