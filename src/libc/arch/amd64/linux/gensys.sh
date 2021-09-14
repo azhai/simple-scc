26a9351dfdfb784400792a06ceb6b76fda488c03
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-09-14 14:47:48 +0200

    libc: Update arch/amd64/linux
    This code is being updated out of the tree of scc and
    it is time to synchroniza both copies now.

--- src/libc/arch/amd64/linux/gensys.sh
+++ src/libc/arch/amd64/linux/gensys.sh
@@ -8,17 +8,19 @@
 sed -n "
      s/[ 	]*#.*//
      /$1/p" syscall.lst |
-while read num name
+while read num name nargs
 do
-
 cat <<EOF > $name.s
 	.file	"$name.s"
 
 	.globl	$name
 $name:
+	`case $nargs in 4|5|6)
+		echo "movq       %rcx,%r10"
+                ;;
+        esac`
 	movq	\$$num,%rax
 	syscall
 	jmp	_cerrno
 EOF
-
 done
