7a0162ccab2a49e56e2da35d3929fab28b7173cf
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-09-14 17:29:05 +0200

    libc: Syncronize syscall.h
    Several functions with prototypes in syscall.h
    were moved to specific system directories but
    the old and not standard definitions were still
    in syscall.h generating additonal and not compatible
    declarations.

--- src/libc/syscall.h
+++ src/libc/syscall.h
@@ -1,8 +1,6 @@
 extern int _brk(void *);
 extern void _exit(int);
 extern int _close(int);
-extern int _getpid(void);
-extern int _kill(int, int);
 extern int _open(const char *, int, int);
 extern int _read(int, void *, size_t);
 extern int _unlink(const char *);
