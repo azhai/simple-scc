02bbb44a56212188031129423a670ffec1554ed5
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-10-26 18:21:19 +0200

    ar: Add whitespace change
    Align comments in the file.

--- include/scc/scc/ar.h
+++ include/scc/scc/ar.h
@@ -5,11 +5,11 @@
 
 
 struct ar_hdr {
-        char ar_name[SARNAM];           /* name */
-        char ar_date[12];               /* modification time */
-        char ar_uid[6];                 /* user id */
-        char ar_gid[6];                 /* group id */
-        char ar_mode[8];                /* octal file permissions */
-        char ar_size[10];               /* size in bytes */
-        char ar_fmag[2];                /* consistency check */
+        char ar_name[SARNAM];      /* name */
+        char ar_date[12];          /* modification time */
+        char ar_uid[6];            /* user id */
+        char ar_gid[6];            /* group id */
+        char ar_mode[8];           /* octal file permissions */
+        char ar_size[10];          /* size in bytes */
+        char ar_fmag[2];           /* consistency check */
 };
