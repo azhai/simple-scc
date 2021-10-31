541539b33afc0c198e44ecce36b6b0f2030c6b3b
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-10-31 18:26:03 +0100

    tests/cc: Fix test 0142-char-const
    The test had several bugs and after fixing them it works
    without problems.

--- tests/cc/execute/0142-char-const.c
+++ tests/cc/execute/0142-char-const.c
@@ -9,32 +9,32 @@ main(void)
 		return 1;
 
 	uc = '\x23';
-	if (uc != 36)
-		return 1;
+	if (uc != 35)
+		return 2;
 
 	uc = 1u;
-	if (uc != (1025 & 0xFF)
+	if (uc != (1025 & 0xFF))
 		return 1;
 
 	uc = 'A';
 	if (uc != 0x41)
-		return 1;
+		return 3;
 
 	sc = -1;
 	if ((sc & 0xFF) != 0xFF)
-		return 1;
+		return 4;
 
 	sc = '\x23';
-	if (sc != 36)
-		return 1;
+	if (sc != 35)
+		return 5;
 
 	sc = 1u;
-	if (uc != (1025 & 0xFF)
-		return 1;
+	if (sc != (1025 & 0xFF))
+		return 6;
 
 	sc = 'A';
-	if (uc != 0x41)
-		return 1;
+	if (sc != 0x41)
+		return 7;
 
 	return 0;
 }
