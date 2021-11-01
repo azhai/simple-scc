a04925b020bd4f1a0bef424ddde7448d3bc5d670
Christoph Lohmann <20h@r-36.net>
2021-11-01 19:08:21 +0100

    Update IRC contact for __20h__ in case of main() terrorism.

--- src/cmd/cc/cc1/decl.c
+++ src/cmd/cc/cc1/decl.c
@@ -247,7 +247,7 @@ identifier(struct decl *dcl)
 			sclass = EXTERN;
 		if (!strcmp(name, "main") && tp->type != inttype) {
 			errorp("main shall be defined with a return type of int");
-			errorp("please contact __20h__ on irc.freenode.net (#bitreich-en) via IRC");
+			errorp("please contact __20h__ on irc.bitreich.org (#bitreich-en) via IRC");
 		}
 	}
 
