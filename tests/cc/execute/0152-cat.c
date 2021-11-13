56278d4ed0aeb9e8896082e9aefdded9e9228a02
Roberto E. Vargas Caballero <k0ga@shike2.com>
2021-11-13 08:21:25 +0100

    tests/cc: Improve test 0152-cat.c
    Returning different values in every case helps while debugging.

--- tests/cc/execute/0152-cat.c
+++ tests/cc/execute/0152-cat.c
@@ -3,12 +3,19 @@
 #define FOO foo
 #define BAR bar
 
+int foobar;
+
 int
 main(void)
 {
-	int foo, bar, foobar;
+	int foo = 1, bar = 0;
 
-	CAT(foo,bar) = foo + bar;
-	XCAT(FOO,BAR) = foo + bar;
+	CAT(foo,bar) += foo + bar;
+	if (foobar != 1)
+		return 1;
+	XCAT(FOO,BAR) += foo + bar;
+	if (foobar != 2)
+		return 2;
+
 	return 0;
 }
