c8b98df43be4c4b8bd2026d517b4962da91a9224
Roberto E. Vargas Caballero <roberto.vargas@midokura.com>
2022-08-16 13:39:41 +0200

    libc/fputs: Fix return value
    When an empty string was passed to fputs it returned
    an uninitialized value.

--- src/libc/stdio/fputs.c
+++ src/libc/stdio/fputs.c
@@ -3,9 +3,11 @@
 #undef fputs
 
 int
-fputs(const char * restrict bp, FILE * restrict fp)
+fputs(const char *restrict bp, FILE *restrict fp)
 {
 	int r, ch;
+
+	r = 0;
 
 	while ((ch = *bp++) != '\0')
 		r = putc(ch, fp);
