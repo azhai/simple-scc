81916340a7c090c9f3ddf7a3cafc2cbbaaf5ea71
Roberto E. Vargas Caballero <k0ga@shike2.com>
2022-10-24 19:43:04 +0200

    libc: undef assert at the beginning of assert.h
    assert.h can be included several times with different values
    of NDEBUG.

--- include/assert.h
+++ include/assert.h
@@ -1,7 +1,8 @@
+#undef assert
+
 extern void __assert(char *, char *, long);
 extern void assert(int);
 
-#undef assert
 #ifndef NDEBUG
 # define assert(exp) ((exp) ? (void) 0 : __assert(#exp, __FILE__, __LINE__))
 #else
