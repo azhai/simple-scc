e91b51fe19fc58e4814eed28fda80ed3990078ea
Roberto E. Vargas Caballero <k0ga@shike2.com>
2022-10-26 13:46:27 +0200

    tests/cc: Enable error/0029-eof.c test

--- tests/cc/error/0029-eof.c
+++ tests/cc/error/0029-eof.c
@@ -1,2 +1,11 @@
+/*
+PATTERN:
+0029-eof.c:11: error: unexpected '<EOF>'
+0029-eof.c:11: warning: type defaults to 'int' in declaration
+0029-eof.c:11: error: expected ')' before '<EOF>'
+0029-eof.c:11: error: expected ';' before '<EOF>'
+.
+*/
+
 int
 main(
