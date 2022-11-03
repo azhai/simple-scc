eee36acd2143365c7c8094175dd425158b4944d4
Roberto E. Vargas Caballero <k0ga@shike2.com>
2022-11-03 07:05:52 +0100

    libc/string: Use explicit cast in strcspn()
    Signess of base types is important for pointer type
    checks.

--- src/libc/string/strcspn.c
+++ src/libc/string/strcspn.c
@@ -5,8 +5,8 @@
 size_t
 strcspn(const char *s1, const char *s2)
 {
-	const unsigned char *s = s1;
-	const unsigned char *reject = s2;
+	const unsigned char *s = (const unsigned char *) s1;
+	const unsigned char *reject = (const unsigned char *) s2;
 	size_t n;
 	unsigned ch;
 	char map[__NUMCHARS] = {0};
