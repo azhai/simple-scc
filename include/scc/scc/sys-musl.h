azhai/simple-scc

commit 631357bb9184f76227d82dab74144118aaca3ad0

driver/posix: Add template for assembler cmd
Having a template for as allows to support non conforming assemblers
like for example clang using the option -x assembler.

include/scc/scc/sys-musl.h
@@ -1,3 +1,6 @@
+#define LDBIN "ld"
+#define ASBIN "as"
+
 /* configure below your standard sys include paths */
 char *sysincludes[] = {
 	"%p/include/",
@@ -24,4 +27,10 @@ char *ldcmd[] = {
 	"-lgcc",
 	"-lgcc_eh",
 	NULL
+};
+
+/* configure below your system assembler command line */
+char *ascmd[] = {
+        "-o", "%o"
+        NULL
 };

include/scc/scc/sys-scc_clang.h
@@ -1,5 +1,5 @@
 #define LDBIN "ld"
-#define ASBIN "as"
+#define ASBIN "clang"
 
 /* configure below your standard sys include paths */
 char *sysincludes[] = {
@@ -26,6 +26,9 @@ char *ldcmd[] = {
 
 /* configure below your system assembler command line */
 char *ascmd[] = {
-	"-o", "%o",
+	"-x","assembler",
+	"-c",
+	"-o","%o",
+	"-",
 	NULL
 };

src/cmd/cc/posix/cc.c
@@ -56,8 +56,8 @@ static struct tool {
 	[TEEQBE] = {.bin = "tee"},
 	[QBE]    = {.bin = "qbe"},
 	[TEEAS]  = {.bin = "tee"},
-	[AS]     = {.bin = "as"},
-	[LD]     = {.bin = "ld"},
+	[AS]     = {.bin = ASBIN},
+	[LD]     = {.bin = LDBIN},
 };
 
 char *argv0;
@@ -139,7 +139,7 @@ addarg(int tool, char *arg)
 			p = abi;
 			break;
 		case 'o':
-			p = outfile;
+			p = t->outfile;
 			break;
 		default:
 			buff[cnt++] = *arg;
@@ -244,6 +244,8 @@ settool(int tool, char *infile, int nexttool)
 			die("scc-cc: target tool path is too long");
 		break;
 	case LD:
+		if (!outfile)
+			outfile = "a.out";
 		t->outfile = xstrdup(outfile);
 		if (gflag)
 			addarg(tool, "-g");
@@ -274,8 +276,9 @@ settool(int tool, char *infile, int nexttool)
 		t->outfile = xstrdup(objfile);
 		if (gflag)
 			addarg(tool, "-g");
-		addarg(tool, "-o");
-		addarg(tool, t->outfile);
+		for (n = 0; ascmd[n]; n++)
+			addarg(tool, ascmd[n]);
+                break;
 		break;
 	default:
 		break;
@@ -656,9 +659,6 @@ operand:
 		if ((devnullfd = open("/dev/null", O_WRONLY)) < 0)
 			fputs("scc-cc: could not open /dev/null\n", stderr);
 	}
-
-	if (!outfile)
-		outfile = "a.out";
 
 	if (!(tmpdir = getenv("TMPDIR")) || !tmpdir[0])
 		tmpdir = ".";

commit 8f382566b9ee9d121277e4c45708bbd40c45a98b

driver: Fix musl template
Commit 631357bb9 introduced a bug when musl was used because a
comma was missed.

include/scc/scc/sys-musl.h
@@ -31,6 +31,6 @@ char *ldcmd[] = {
 
 /* configure below your system assembler command line */
 char *ascmd[] = {
-        "-o", "%o"
+        "-o", "%o",
         NULL
 };