83a44c0292f5ac37c809b3ac00600a831f97099c
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-17 14:45:49 +0100

    tests/make: Split 0002-fflag.sh
    It is better to have only one test case per file instead
    of sharing the script betwen different test cases.

--- tests/make/execute/0003-fflag.sh
+++ tests/make/execute/0003-fflag.sh
@@ -5,7 +5,7 @@ trap 'rm -f $tmp1 $tmp2' EXIT INT QUIT HUP TERM
 tmp1=tmp1.$$
 tmp2=tmp2.$$
 
-scc-make -f test.mk print-cc print-makeflags > $tmp1 2>&1
+scc-make -ftest.mk print-cc print-makeflags > $tmp1 2>&1
 
 cat <<EOF > $tmp2
 c99
