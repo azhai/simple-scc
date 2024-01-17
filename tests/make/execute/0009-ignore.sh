6b384cc12e8cd8f01bf66cc59f367b146e16b31a
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-17 19:28:34 +0100

    tests/make: Simplify 0009-ignore.sh
    We can use a here-doc instead of creating a new file.

--- tests/make/execute/0009-ignore.sh
+++ tests/make/execute/0009-ignore.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 
-trap 'rm -f $tmp1 $tmp2 $tmp3' EXIT INT QUIT HUP TERM
+trap 'rm -f $tmp1 $tmp2' EXIT INT QUIT HUP TERM
 
 tmp1=tmp1.$$
 tmp2=tmp2.$$
-tmp3=tmp3.$$
-
-cat <<EOF > $tmp2
-EOF
-
-cat <<EOF > $tmp3
-.IGNORE:
-
-error:
-	no-valid-program
-EOF
 
 cat <<EOF > $tmp2
 no-valid-program
@@ -22,6 +11,11 @@ sh: 1: no-valid-program: not found
 make: error: don't know how to make hello
 EOF
 
-scc-make -f $tmp3 error hello > $tmp1 2>&1
+scc-make -f - error hello > $tmp1 2>&1 <<EOF
+.IGNORE:
+
+error:
+	no-valid-program
+EOF
 
 diff $tmp1 $tmp2
