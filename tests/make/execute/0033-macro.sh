e85926dae9ee6bfd68553c4294fbd9969a3d50a7
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-18 13:53:15 +0100

    tests/make: Add 0033-macro test

---
+++ tests/make/execute/0033-macro.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2' EXIT INT QUIT TERM HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+echo pcc > $tmp2
+
+scc-make -f test.mk CC=pcc print-cc > $tmp1 2>&1
+
+diff $tmp1 $tmp2
