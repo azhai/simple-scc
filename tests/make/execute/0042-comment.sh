8fd968ed136c188ba61fb876dc134a96df445f79
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-18 15:53:11 +0100

    tests/make: Add 0042-comment test

---
+++ tests/make/execute/0042-comment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+trap 'rm $tmp1 $tmp2' EXIT INT QUIT TERM HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+echo Hello World! > $tmp2
+
+scc-make -f - <<'EOF' > $tmp1
+    # blank line
+all:
+	@echo Hello World!
+EOF
+
+diff $tmp1 $tmp2
