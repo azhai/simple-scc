94e1a7cda3c8cc1e8c2c28ebdbbe090a85a3c863
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-19 18:38:01 +0100

    tests/make: Add 0062-macro test

---
+++ tests/make/execute/0062-macro.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2' EXIT INT TERM QUIT HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+echo Hello World!> $tmp2
+
+scc-make -f- all <<'EOF' > $tmp1 2>&1
+TARGET = all
+
+$(TARGET):
+	@echo Hello World!
+EOF
+
+diff $tmp1 $tmp2
