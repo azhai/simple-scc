706a8652c01e980c1e2be2673306c6341fcceb60
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-19 13:21:52 +0100

    tests/make: Add 0054-rule test

---
+++ tests/make/execute/0054-rule.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2' EXIT INT QUIT TERM HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+cat > $tmp2 <<EOF
+Hello World!
+bye
+fail
+EOF
+
+(set -e
+ scc-make -f- <<'EOF'
+.POSIX:
+
+.IGNORE:
+
+all:
+	@echo Hello World!;\
+	false;\
+	echo bye
+EOF
+echo fail) > $tmp1 2> /dev/null
+
+diff $tmp1 $tmp2
