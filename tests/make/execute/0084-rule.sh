d0fb80726024f3921651e219456375bbdef5cfab
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-21 00:10:14 +0100

    tests/make: Add 0084-rule test

---
+++ tests/make/execute/0084-rule.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2 f f1 f2 f3' EXIT INT TERM QUIT HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+echo f3 > $tmp2
+
+touch -t 200711121015 f1 f2
+touch -t 200711121016 f 
+touch -t 200711121017 f3 
+
+scc-make -f- <<'EOF' > $tmp1 2>&1
+f: f1 f2 f3
+	@echo $?
+EOF
+
+diff $tmp1 $tmp2
