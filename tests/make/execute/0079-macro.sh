b161f67e72e5a9c1ac36a2197b27291dadedccb1
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-20 23:12:04 +0100

    tests/make: Add 0079-macro test

---
+++ tests/make/execute/0079-macro.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2' EXIT INT QUIT TERM HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+echo SHELL=/bin/sh > $tmp2
+SHELL=foo scc-make -f- <<'EOF'
+SHELL = /bin/sh
+
+all:
+	@echo $$SHELL
+EOF
