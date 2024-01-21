ba45b797ed7a87840029720f7386e901fbf022dc
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-01-21 12:06:16 +0100

    tests/make: Add 0090-comment

---
+++ tests/make/execute/0090-comment.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+trap 'rm -f $tmp1 $tmp2' EXIT INT TERM QUIT HUP
+
+tmp1=tmp1.$$
+tmp2=tmp2.$$
+
+cat >$tmp2 <<EOF
+Hello World!
+EOF
+
+scc-make -f- <<'EOF' > $tmp1 2>&1
+all:     # This is a comment
+	@echo Hello World!
+EOF
+
+diff $tmp1 $tmp2
