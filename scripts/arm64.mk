1156602edeb3876e50b1b8a3f038f0b4cae41e27
Roberto E. Vargas Caballero <k0ga@shike2.com>
2024-03-12 18:43:33 +0100

    build: Fix scripts/arm64.mk
    This script was missing the targets for the aarch64 names, covering
    only the cases where arm64 was used.

--- scripts/arm64.mk
+++ scripts/arm64.mk
@@ -1,8 +1,8 @@
-arm64:
+aarch64 arm64:
 	+@$(MAKE) `$(SCRIPTDIR)/config` CONF=arm64-linux libc libcrt
 
-install-arm64: arm64
+install-arm64 install-aarch64: arm64
 	$(SCRIPTDIR)/install -p $(SCRIPTDIR)/proto.arm64 $(ROOT)
 
-uninstall-arm64:
+uninstall-arm64 uninstall-arm64:
 	$(SCRIPTDIR)/uninstall -p $(SCRIPTDIR)/proto.arm64 $(ROOT)
